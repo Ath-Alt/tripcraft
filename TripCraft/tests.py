65b425d4212e93da124633ed14b1cda0463594d0
Ath-Alt <atheen.altouaimi@gmail.com>
2025-07-29 11:50:57 +0300

    Fix tests.py

--- TripCraft/tests.py
+++ TripCraft/tests.py
@@ -2,6 +2,6 @@ from django.test import TestCase
 from django.urls import reverse
 
 class ViewTest(TestCase):
-    def home_page_status(self):
+    def test_home_page(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
