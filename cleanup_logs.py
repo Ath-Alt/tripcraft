6d870e2c27514ea988222b19f74e99f727083c67
Ath-Alt <atheen.altouaimi@gmail.com>
2025-08-10 13:46:20 +0300

    Fix elastic ip

--- cleanup_logs.py
+++ cleanup_logs.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 from elasticsearch import Elasticsearch
 
-es = Elasticsearch("http://localhost:9200")
+es = Elasticsearch("http://192.168.0.1:9200")
 
 old_time = datetime.now() - timedelta(days=1)
 formatted_old_time = old_time.strftime("%Y-%m-%dT%H:%M:%S")
